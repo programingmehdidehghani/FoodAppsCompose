e9d51b8a79bc1e2140feee8fd153a65b7bd6595f
mehdi <dehghani_m@rocketmail.com>
2022-11-25 14:19:00 +0330

    add customview

--- app/src/main/java/com/example/foodapps/RecipeListFragment.kt
+++ app/src/main/java/com/example/foodapps/RecipeListFragment.kt
@@ -4,8 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 
 
@@ -16,10 +26,22 @@ class RecipeListFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = ComposeView(requireContext())
-        view.apply {
-            setContent {
-                Text("hey look a composable")
+        val view = inflater.inflate(R.layout.fragment_recipe_list,container,false)
+        view.findViewById<ComposeView>(R.id.compose_view).setContent {
+            
+            Column(modifier = androidx.compose.ui.Modifier
+                .border(border = BorderStroke(1.dp, Color.Black))
+                .padding(16.dp)
+            ) {
+                Text("This is Composable Inside in")
+                Spacer(modifier = androidx.compose.ui.Modifier.padding(10.dp))
+                CircularProgressIndicator()
+                Spacer(modifier = androidx.compose.ui.Modifier.padding(10.dp))
+                Text("Neat")
+                Spacer(modifier = androidx.compose.ui.Modifier.padding(10.dp))
+
+                val customView = HorizontalDottedProgress(LocalContext.current)
+                AndroidView(factory = {customView})
             }
         }
         return view
