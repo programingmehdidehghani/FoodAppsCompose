a396a6939d80411447b14d03ec61218976672c70
mehdi <dehghani_m@rocketmail.com>
2022-11-25 13:11:16 +0330

    add progress custom view

--- app/src/main/java/com/example/foodapps/MainActivity.kt
+++ app/src/main/java/com/example/foodapps/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container,RecipeListFragment())
             .commit()
--- app/src/main/java/com/example/foodapps/RecipeListFragment.kt
+++ app/src/main/java/com/example/foodapps/RecipeListFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 
 
@@ -14,8 +16,12 @@ class RecipeListFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_recipe_list
-         , container , false)
+        val view = ComposeView(requireContext())
+        view.apply {
+            setContent {
+                Text("hey look a composable")
+            }
+        }
         return view
     }
 }
