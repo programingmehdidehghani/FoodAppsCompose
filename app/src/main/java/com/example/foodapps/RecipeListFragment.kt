aabce388396d3175cfa7a7b4c192ba4269f71f92
mehdi <dehghani_m@rocketmail.com>
2022-11-25 16:10:48 +0330

    try make fragment

--- app/src/main/java/com/example/foodapps/RecipeListFragment.kt
+++ app/src/main/java/com/example/foodapps/RecipeListFragment.kt
@@ -9,12 +9,17 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 
@@ -26,24 +31,21 @@ class RecipeListFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_recipe_list,container,false)
-        view.findViewById<ComposeView>(R.id.compose_view).setContent {
-            
-            Column(modifier = androidx.compose.ui.Modifier
-                .border(border = BorderStroke(1.dp, Color.Black))
-                .padding(16.dp)
-            ) {
-                Text("This is Composable Inside in")
-                Spacer(modifier = androidx.compose.ui.Modifier.padding(10.dp))
-                CircularProgressIndicator()
-                Spacer(modifier = androidx.compose.ui.Modifier.padding(10.dp))
-                Text("Neat")
-                Spacer(modifier = androidx.compose.ui.Modifier.padding(10.dp))
-
-                val customView = HorizontalDottedProgress(LocalContext.current)
-                AndroidView(factory = {customView})
-            }
-        }
-        return view
+         return ComposeView(requireContext()).apply {
+             setContent {
+                 Column(modifier = Modifier.padding(16.dp)) {
+                     Text(
+                         text = "Recipe List",
+                         style = TextStyle(
+                             fontSize = 21.sp
+                         )
+                     )
+                     Spacer(modifier = Modifier.padding(10.dp))
+                     Button(onClick = { /*TODO*/ }) {
+                         
+                     }
+                 }
+             }
+         }
     }
 }
