17f43134a1daabb3d1e80c9fd6809fcf123ee7e7
mehdi <dehghani_m@rocketmail.com>
2022-12-15 15:17:47 +0330

    continue make varriavle data class

--- app/src/main/java/com/example/foodapps/domain/model/Recipe.kt
+++ app/src/main/java/com/example/foodapps/domain/model/Recipe.kt
@@ -2,10 +2,19 @@ package com.example.foodapps.domain.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.util.SplittableRandom
 
 
 @Parcelize
 data class Recipe(
-    val id:Int
+    val id: Int? = null,
+    val title: String? = null,
+    val publisher: String? = null,
+    val featuredImage: String? = null,
+    val rating: Int? = 0,
+    val sourceUrl: String? = null,
+    val description: String? = null,
+    val cookingInstructions: String? = null,
+    val ingredients: List<String> = listOf()
 ) : Parcelable{
 }
