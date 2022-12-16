c09213e167418782dbb20cec33e74f2439b96716
mehdi <dehghani_m@rocketmail.com>
2022-12-16 15:03:01 +0330

    continue add in model

--- app/src/main/java/com/example/foodapps/network/model/RecipeNetworkMapper.kt
+++ app/src/main/java/com/example/foodapps/network/model/RecipeNetworkMapper.kt
@@ -5,9 +5,18 @@ import com.example.foodapps.domain.util.EntityMapper
 
 class RecipeNetworkMapper : EntityMapper<RecipeNetworkEntity,Recipe> {
 
-    
+
     override fun mapFromEntity(entity: RecipeNetworkEntity): Recipe {
-        TODO("Not yet implemented")
+        return Recipe(
+            id = entity.pk,
+            title = entity.title,
+            featuredImage = entity.featuredImage,
+            rating = entity.rating,
+            publisher = entity.publisher,
+            sourceUrl = entity.sourceUrl,
+            description = entity.description,
+            cookingInstructions = entity.cookingInstructions
+        )
     }
 
     override fun mapToEntity(domainModel: Recipe): RecipeNetworkEntity {
