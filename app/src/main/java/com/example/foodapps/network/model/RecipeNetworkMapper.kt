388cc36b60ac2fab041d8f25768fc07cfd5be4bd
mehdi <dehghani_m@rocketmail.com>
2022-12-16 15:00:12 +0330

    implement method entity

--- app/src/main/java/com/example/foodapps/network/model/RecipeNetworkMapper.kt
+++ app/src/main/java/com/example/foodapps/network/model/RecipeNetworkMapper.kt
@@ -4,4 +4,15 @@ import com.example.foodapps.domain.model.Recipe
 import com.example.foodapps.domain.util.EntityMapper
 
 class RecipeNetworkMapper : EntityMapper<RecipeNetworkEntity,Recipe> {
+
+    
+    override fun mapFromEntity(entity: RecipeNetworkEntity): Recipe {
+        TODO("Not yet implemented")
+    }
+
+    override fun mapToEntity(domainModel: Recipe): RecipeNetworkEntity {
+        TODO("Not yet implemented")
+    }
+
+
 }
