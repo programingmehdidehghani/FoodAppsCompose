7ff3028749739bc2ff00d38d8adb9441690a18ee
mehdi <dehghani_m@rocketmail.com>
2022-12-16 14:57:27 +0330

    make recipeNteworkMapper

---
+++ app/src/main/java/com/example/foodapps/network/model/RecipeNetworkMapper.kt
@@ -0,0 +1,7 @@
+package com.example.foodapps.network.model
+
+import com.example.foodapps.domain.model.Recipe
+import com.example.foodapps.domain.util.EntityMapper
+
+class RecipeNetworkMapper : EntityMapper<RecipeNetworkEntity,Recipe> {
+}
