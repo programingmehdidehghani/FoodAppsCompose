c4e56aee3033ff483677cb6b3a7ff91001d3d908
mehdi <dehghani_m@rocketmail.com>
2023-01-07 14:15:56 +0330

    continue make retrofit

--- app/src/main/java/com/example/foodapps/network/RecipeService.kt
+++ app/src/main/java/com/example/foodapps/network/RecipeService.kt
@@ -1,4 +1,15 @@
 package com.example.foodapps.network
 
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
 interface RecipeService {
+
+    @GET("search")
+    suspend fun search(
+        @Header("Authorization") token: String,
+        @Query("page") page : Int,
+        @Query("query") query : String
+    )
 }
