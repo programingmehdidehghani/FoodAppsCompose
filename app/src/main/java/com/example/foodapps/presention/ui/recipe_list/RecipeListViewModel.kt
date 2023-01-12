36afe2d18662207e94eb2eba436bd555925b67e3
mehdi <dehghani_m@rocketmail.com>
2023-01-12 21:43:19 +0330

    test mutable live data

--- app/src/main/java/com/example/foodapps/presention/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/example/foodapps/presention/ui/recipe_list/RecipeListViewModel.kt
@@ -1,22 +1,34 @@
 package com.example.foodapps.presention.ui.recipe_list
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.foodapps.domain.model.Recipe
 import com.example.foodapps.repository.RecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Named
 
 @HiltViewModel
 class RecipeListViewModel
- constructor(
-   private val repository:  RecipeRepository,
-   private @Named("auth_token") val token: String
- ) : ViewModel() {
+constructor(
+    private val repository: RecipeRepository,
+    private @Named("auth_token") val token: String
+) : ViewModel() {
 
-     init {
+    private val _recipes: MutableLiveData<List<Recipe>> = MutableLiveData()
+    val recipe: LiveData<List<Recipe>> = _recipes
 
-     }
+    init {
+      viewModelScope.launch {
+          val result = repository.search(
+              token = token,
+              page = 1,
+              query = "chicken"
+          )
+          _recipes.value = result
+      }
+    }
 
-    fun getRepo() = repository
-
-    fun getToken() = token
 }
--- app/src/main/java/com/example/foodapps/repository/RecipeRepository.kt
+++ app/src/main/java/com/example/foodapps/repository/RecipeRepository.kt
@@ -4,7 +4,7 @@ import com.example.foodapps.domain.model.Recipe
 
 interface RecipeRepository {
 
-    suspend fun search(token :String,id: Int,query: String): List<Recipe>
+    suspend fun search(token :String,page: Int,query: String): List<Recipe>
 
     suspend fun get(token: String,id: Int): Recipe
 }
