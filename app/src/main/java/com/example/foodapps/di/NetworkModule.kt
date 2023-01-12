13bcaf1af27ebe2f83857a401614235d74b882bc
mehdi <dehghani_m@rocketmail.com>
2023-01-12 20:53:48 +0330

    make instance retrofit

--- app/src/main/java/com/example/foodapps/di/NetworkModule.kt
+++ app/src/main/java/com/example/foodapps/di/NetworkModule.kt
@@ -1,4 +1,35 @@
 package com.example.foodapps.di
 
+import com.example.foodapps.network.RecipeService
+import com.example.foodapps.network.model.RecipeDtoMapper
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRecipeMapper() : RecipeDtoMapper{
+        return RecipeDtoMapper()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRecipeService(): RecipeService{
+        return Retrofit.Builder()
+            .baseUrl("https://food2fork.ca/api/recipe/")
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+            .create(RecipeService::class.java)
+    }
 }
