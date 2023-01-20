f06b3b90dfa78fc95c2142ce05f4c88afda82f5a
mehdi <dehghani_m@rocketmail.com>
2023-01-20 16:21:49 +0330

    try make class load image

--- app/src/main/java/com/example/foodapps/utli/ImageUtli.kt
+++ app/src/main/java/com/example/foodapps/utli/ImageUtli.kt
@@ -1,2 +1,21 @@
 package com.example.foodapps.utli
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import com.bumptech.glide.Glide
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun loadPicture(
+    url: String,
+    @DrawableRes defaultImage: Int
+): MutableState<Bitmap?>{
+
+    val bitmapState : MutableState<Bitmap?> = mutableStateOf(null)
+
+    Glide.with(AmbientContext.current)
+}
