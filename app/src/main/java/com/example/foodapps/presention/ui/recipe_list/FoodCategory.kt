c5c2ff6e095be007160969cc7e67c0eb8398affa
mehdi <dehghani_m@rocketmail.com>
2023-01-24 15:56:42 +0330

    make varriable enum food

--- app/src/main/java/com/example/foodapps/presention/ui/recipe_list/FoodCategory.kt
+++ app/src/main/java/com/example/foodapps/presention/ui/recipe_list/FoodCategory.kt
@@ -1,2 +1,13 @@
 package com.example.foodapps.presention.ui.recipe_list
 
+enum class FoodCategory(val value: String){
+
+    CHICKEN("Chicken"),
+    BEEF("Beef"),
+    SOUP("Soup"),
+    DESSERT("Dessert"),
+    VEGETARIAN("Vegetarian"),
+    MILK("Milk"),
+    VEGAN("Vegan"),
+    
+}
