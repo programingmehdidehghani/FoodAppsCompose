d50d0910236d9b33f2b3c111e2c4d2df4b8df6d8
mehdi <dehghani_m@rocketmail.com>
2023-01-24 15:17:27 +0330

    key bprd options

--- app/src/main/java/com/example/foodapps/presention/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/foodapps/presention/ui/recipe_list/RecipeListFragment.kt
@@ -7,12 +7,14 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
@@ -55,7 +57,10 @@ class RecipeListFragment : Fragment(){
                                },
                                label = {
                                    Text(text = "Search")
-                               }
+                               },
+                               keyboardOptions = KeyboardOptions(
+                                   keyboardType = KeyboardType.Text
+                               )
                            )
                            Spacer(modifier = Modifier.padding(10.dp))
                            LazyColumn{
