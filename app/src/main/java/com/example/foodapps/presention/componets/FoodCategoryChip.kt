34fa370679996bdba5d0610165b2c1e54a2b9058
mehdi <dehghani_m@rocketmail.com>
2023-02-05 20:46:38 +0330

    continue make compose category

--- app/src/main/java/com/example/foodapps/presention/componets/FoodCategoryChip.kt
+++ app/src/main/java/com/example/foodapps/presention/componets/FoodCategoryChip.kt
@@ -1,10 +1,14 @@
 package com.example.foodapps.presention.componets
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.foodapps.presention.ui.recipe_list.FoodCategory
 
@@ -18,6 +22,16 @@ fun FoodCategoryChip(
         elevation = 8.dp,
         shape = MaterialTheme.shapes.large
     ) {
-
+        Row(
+            modifier = Modifier
+                .clickable(onClick = {onExecuteSearch(category)})
+        ) {
+            Text(
+                text = category,
+                style = MaterialTheme.typography.body2,
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
     }
 }
