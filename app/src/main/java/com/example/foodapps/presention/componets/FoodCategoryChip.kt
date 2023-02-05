f47685ac1c1ac61b6671eb14cd5f457b059c6fa4
mehdi <dehghani_m@rocketmail.com>
2023-02-05 20:34:06 +0330

    continue make compose foodcategory

--- app/src/main/java/com/example/foodapps/presention/componets/FoodCategoryChip.kt
+++ app/src/main/java/com/example/foodapps/presention/componets/FoodCategoryChip.kt
@@ -1,2 +1,23 @@
 package com.example.foodapps.presention.componets
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.foodapps.presention.ui.recipe_list.FoodCategory
+
+@Composable
+fun FoodCategoryChip(
+    category: String,
+    onExecuteSearch: (String) -> Unit
+){
+    Surface(
+        modifier = Modifier.padding(end = 8.dp),
+        elevation = 8.dp,
+        shape = MaterialTheme.shapes.large
+    ) {
+
+    }
+}
