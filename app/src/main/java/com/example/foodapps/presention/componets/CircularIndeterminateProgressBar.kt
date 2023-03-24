99e091c3c5375aa798060174350d3426d2daa058
mehdi <dehghani_m@rocketmail.com>
2023-03-24 17:04:03 +0430

    fix warning compose

--- app/src/main/java/com/example/foodapps/presention/componets/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/example/foodapps/presention/componets/CircularIndeterminateProgressBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CircularIndeterminateProgressBar(
--- app/src/main/java/com/example/foodapps/presention/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/example/foodapps/presention/ui/recipe_list/RecipeListViewModel.kt
@@ -74,6 +74,6 @@ class RecipeListViewModel
     }
 
     fun onChangeCategoryScrollPosition(position: Int){
-        categoryScrollPosition = position
+        categoryScrollPosition = position.toFloat()
     }
 }
