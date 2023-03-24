4563e16d1e063fffa2b12430d1118966c6d1cb47
mehdi <dehghani_m@rocketmail.com>
2023-03-24 17:25:05 +0430

    add constran in compose

--- app/src/main/java/com/example/foodapps/presention/componets/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/example/foodapps/presention/componets/CircularIndeterminateProgressBar.kt
@@ -1,31 +1,43 @@
 package com.example.foodapps.presention.componets
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 
 @Composable
 fun CircularIndeterminateProgressBar(
-    isDisplayed : Boolean
-){
-    if (isDisplayed){
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            horizontalArrangement = Arrangement.Center
+    isDisplayed: Boolean
+) {
+    if (isDisplayed) {
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize()
         ) {
+            val (progressBar, text) = createRefs()
             CircularProgressIndicator(
-                color = MaterialTheme.colors.primary
+                modifier = Modifier.constrainAs(progressBar) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }, color = MaterialTheme.colors.primary
+            )
+            Text(
+                text = "Loading ....",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 
+                )
             )
         }
-
     }
 
 }
